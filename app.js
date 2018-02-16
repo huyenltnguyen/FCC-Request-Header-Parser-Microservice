7db1247a8af06b2b1424b77d3a70e560713b3648
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-02-16 19:07:21 +0700

    reformat coding style

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ app.use(express.static('public'));
 // ROUTES
 //===========
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/api/whoami', function(req, res) {
-	var ip = req.ip;
-	var lang = req.headers["accept-language"].match(/.*?(?=,)/).join('');
-	var os = req.headers["user-agent"].match(/\((.*?)\)/)[1];
-	res.json({
-		"ipaddress": ip,
-		"language": lang,
-		"os": os
-	});
+  var ip = req.ip;
+  var lang = req.headers['accept-language'].match(/.*?(?=,)/).join('');
+  var os = req.headers['user-agent'].match(/\((.*?)\)/)[1];
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    os: os
+  });
 });
 
 app.listen(3000, function() {
-	console.log('App is running on port 3000');
+  console.log('App is running on port 3000');
 });
